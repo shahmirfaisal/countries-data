b1d29ae17227b63147dbf63a0dfd600dab23f222
SHAHMIR FAISAL <shahmir049@outlook.com>
2020-02-21 18:28:36 +0500

    Deployed

---
+++ build/precache-manifest.43d1869878166bc52002fab927d7045f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2438437b1ca16ffaa2451d043bea93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c86b96fa36415c28e07",
+    "url": "/static/css/main.048eecbb.chunk.css"
+  },
+  {
+    "revision": "b33f78b15c24192376bb",
+    "url": "/static/js/2.b14d1789.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.b14d1789.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c86b96fa36415c28e07",
+    "url": "/static/js/main.e8f81731.chunk.js"
+  },
+  {
+    "revision": "79165c6bf1101c023ac8",
+    "url": "/static/js/runtime-main.68f93d91.js"
+  },
+  {
+    "revision": "f1bc46400ccecb32ee40bfec3866ff31",
+    "url": "/static/media/angle-down-solid.f1bc4640.svg"
+  }
+]);
